dc01b847e3a4b5751072177b365fe06ad10e3293
southernpitbull <77404848+southernpitbull@users.noreply.github.com>
2025-07-08 02:54:56 -0400

    Delete launch.bat

--- launch.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting The Oracle...
-cd /d "%~dp0"
-".venv\Scripts\python.exe" main.py
-if %errorlevel% neq 0 (
-    echo.
-    echo An error occurred. Press any key to exit...
-    pause > nul
-)
